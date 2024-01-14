dc08ffa549ad043d7f31cf617507adf5dccdd205
infinikkk <tsapik.yehor@lll.kpi.ua>
2024-01-14 20:38:48 +0800

    simplified the function

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,16 +1,6 @@
 // Return an array without duplicates
 'use strict';
 
-const distinct = (array) => {
-  const distArray = [];
-
-  for (const elem of array) {
-    if (!distArray.includes(elem)) {
-      distArray.push(elem);
-    }
-  }
-
-  return distArray;
-};
+const distinct = (array) => [...new Set(array)];
 
 module.exports = distinct;
