5ef3876b18c698c95e17b2194855e9fa45f00b51
infinikkk <tsapik.yehor@lll.kpi.ua>
2024-01-14 19:38:28 +0800

    simplified the function

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,19 +1,6 @@
 // Filter array by type name
 'use strict';
 
-const filterByType = (array, type) => {
-  const remove = [];
-
-  for (const elem of array) {
-    const index = array.indexOf(elem);
-    if (typeof array[index] !== type) {
-      remove.unshift(index);
-    }
-  }
-
-  for (const index of remove) array.splice(index, 1);
-
-  return array;
-};
+const filterByType = (arr, type) => arr.filter((elem) => typeof elem === type);
 
 module.exports = filterByType;
